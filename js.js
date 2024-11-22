fdc462f016d67fe688441e80295c35eb5a857a6d
Marco Peliteiro <83987191+marcofernandes2002@users.noreply.github.com>
2024-11-22 14:53:48 +0100

    Complete step 5

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-const table = document.querySelector("#table");
+//const table = document.querySelector("#table");
 
 const arr = [];
 
@@ -13,13 +13,18 @@ function addBookToLibrary(title, author){
 }
 
 addBookToLibrary('Daily Rituals', 'Mason Currey');
-addBookToLibrary('Digital Minimalism', 'Cal Newport');
+addBookToLibrary('Digital Minimalism', 'Cal Newport' );
 
 function displayBooks(arr){
+  const table= document.createElement("table");
+  table.className ="booksTable";
+  document.body.appendChild(table);
+  
   arr.forEach((element)=>{
     const newRow = document.createElement("tr");
     const tdTitle = document.createElement("td");
     const tdAuthor =      document.createElement("td");
+    const index =      document.createElement("td");
     
     const bookIndex=arr.indexOf(element);
 
@@ -31,20 +36,75 @@ function displayBooks(arr){
     
     tdTitle.textContent = element.title;
     tdAuthor.textContent = element.author;
+    index.textContent = bookIndex;
+    
     newRow.appendChild(tdTitle);
     newRow.appendChild(tdAuthor);
     newRow.appendChild(deleteBtn);
+    newRow.appendChild(index);
     table.appendChild(newRow);
     
     deleteBtn.addEventListener("click",function () { 
       newRow.remove();
+      arr.splice(deleteBtn.dataset.bookindex =bookIndex,1);
+      table.remove();
+  displayBooks(arr);
     });
   });
 };
 
 displayBooks(arr);
 
+const showButton=document.getElementById("showDialog");
+const createBook = document.getElementById("createBook");
+const confirmBtn = document.querySelector("#confirmBtn");
 
+
+showButton.addEventListener("click", () => {
+  createBook.showModal();
+});
+
+confirmBtn.addEventListener("click", (event) => {
+  event.preventDefault(); 
+  
+  const table = document.querySelector(".booksTable");
+  
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  
+  addBookToLibrary(title,author);
+  
+  createBook.close();
+  
+    const newRow = document.createElement("tr");
+    const tdTitle = document.createElement("td");
+    const tdAuthor = document.createElement("td");
+   const index =      document.createElement("td");
+    
+    const bookIndex=(arr.length)-1;
+  
+    const deleteBtn= document.createElement("button");
+    deleteBtn.className = "deleteButton";
+    
+    deleteBtn.dataset.bookindex =bookIndex;
+    
+    tdTitle.textContent = title;
+    tdAuthor.textContent = author;
+    index.textContent = bookIndex;
+  
+  newRow.appendChild(tdTitle);
+    newRow.appendChild(tdAuthor);
+    newRow.appendChild(deleteBtn);
+  newRow.appendChild(index);
+    table.appendChild(newRow);
+    
+    deleteBtn.addEventListener("click",function () { 
+      newRow.remove();
+      arr.splice(deleteBtn.dataset.bookindex =bookIndex,1);
+      table.remove();
+  displayBooks(arr);
+    });
+});
 
               
               
